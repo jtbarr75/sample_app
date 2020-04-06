0444dc713d2d56fd3b7969fad6e23bfb4aadff9f
Jason Barr <Barr75@gmail.com>
2020-04-06 15:24:51 -0700

    fix index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.where(activated: true).paginate(page: params[:page])
+    #@users = User.where(activated: true).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
