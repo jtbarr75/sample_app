d554e09133a914e559de7d4e592eeb9f1134e584
Jason Barr <Barr75@gmail.com>
2020-04-09 15:09:18 -0700

    add microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  has_many :microposts, dependent: :destroy
+  attr_accessor :remember_token#, :activation_token
   before_save :downcase_email
-  before_create :create_activation_digest
+  #before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -42,15 +43,20 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  #activates an account
-  def activate
-    update_columns(activated: true, activated_at: Time.zone.now)
+  #defines a proto feed
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
-  #sends activation email
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
+  # #activates an account
+  # def activate
+  #   update_columns(activated: true, activated_at: Time.zone.now)
+  # end
+
+  # #sends activation email
+  # def send_activation_email
+  #   UserMailer.account_activation(self).deliver_now
+  # end
 
   private
 
@@ -59,9 +65,9 @@ class User < ApplicationRecord
       email.downcase!
     end
 
-    #creates and assigns the activation token and digest
-    def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+    # #creates and assigns the activation token and digest
+    # def create_activation_digest
+    #   self.activation_token = User.new_token
+    #   self.activation_digest = User.digest(activation_token)
+    # end
 end
